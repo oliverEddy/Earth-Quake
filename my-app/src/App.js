e631a7ae15e8eea1f2b61ec12e64871c95989250
Oliver Eddy <olivereddy.developer@gmail.com>
2024-05-16 20:04:25 +1200

    Refactor data fetching and display in App.js, and add new components Header.js and EarthQuakeList.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
 import EarthQuakeList from './components/EarthQuakeList';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-200 p-10 flex flex-col items-center">
       <Header />
       <EarthQuakeList />
     </div>
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,8 @@
+// my-app/tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
